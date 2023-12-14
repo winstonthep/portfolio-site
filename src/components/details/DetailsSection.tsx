8bfd1811dfff98a82221d3dbfaff4428645dbb66
Winston Pantelakos <winstonspantelakos@gmail.com>
2023-12-14 10:03:21 -0500

    adds navbar

--- src/components/details/DetailsSection.tsx
+++ src/components/details/DetailsSection.tsx
@@ -1,7 +1,9 @@
 import { About } from "./About";
+import { Navbar } from "../Navbar";
 export const DetailsSection = () => {
   return (
-    <div className="p-4 h-full">
+    <div className="p-4 h-full relative">
+      <Navbar />
       <About />
     </div>
   )
