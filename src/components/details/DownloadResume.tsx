ce73c939e228de7d779caa26b872cce8f7c9e236
Winston Pantelakos <winstonspantelakos@gmail.com>
2024-01-02 10:19:48 -0500

    updates downloadable resume

--- src/components/details/DownloadResume.tsx
+++ src/components/details/DownloadResume.tsx
@@ -1,6 +1,6 @@
 import { IonIcon } from "@ionic/react";
 import { cloudDownload } from "ionicons/icons";
-import resumeDownload from "../../assets/Winston Pantelakos Resume-December 2023.pdf"
+import resumeDownload from "../../assets/Winston Pantelakos Resume-January 2024.pdf"
 export const DownloadResume = () => {
   return (
     <a className=" h-12 w-16 p-2 rounded-2xl bg-blue-300 hover:bg-white hover:cursor-pointer" href={resumeDownload} download="Winston-Pantelakos-Resume.pdf">
